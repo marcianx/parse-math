0caf79d62f7cfe16a204291a95f5014196051c95
Ashish Myles <marcianx@gmail.com>
2016-07-03 22:31:36 -0400

    Accounted for operator associativity.

--- src/shuntingyard.rs
+++ src/shuntingyard.rs
@@ -28,6 +28,26 @@ const OPS_PREFIX: [char; 1] = ['-'];
 fn is_binary(op_char: char) -> bool {
     OPS_BINARY.contains(&op_char)
 }
+fn is_prefix(op_char: char) -> bool {
+    OPS_PREFIX.contains(&op_char)
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Assoc { Left, Right }
+
+fn assoc(op: &Op) -> Assoc {
+    if let &Op::Binary(ch, _) = op {
+        match ch {
+            '^' => return Assoc::Right,
+            _ => {
+                assert!(['+', '-', '*', '/'].contains(&ch), "Unknown operator associativity {}", ch);
+                return Assoc::Left
+            }
+        };
+    }
+    panic!("Operator {:?} does not have associativity", op)
+}
+
 fn prec(op: &Op) -> i32 {
     match op {
         &Op::Sentinel(_) => 0,
@@ -35,8 +55,15 @@ fn prec(op: &Op) -> i32 {
         &Op::Binary('*', _) | &Op::Binary('/', _) => 2,
         &Op::Prefix('-', _) => 3,
         &Op::Binary('^', _) => 4,
-        _ => panic!(format!("Unexpected operator {:?}", op)),
+        _ => panic!("Unexpected operator {:?}", op),
     }
+}
+
+#[inline(always)]
+fn has_greater_prec(op1: &Op, op2: &Op) -> bool {
+    let prec1 = prec(&op1);
+    let prec2 = prec(&op2);
+    prec1 > prec2 || (prec1 == prec2 && assoc(op1) == Assoc::Left)
 }
 
 impl<'a> ShuntingYard<'a> {
@@ -97,7 +124,7 @@ impl<'a> ShuntingYard<'a> {
                 self.exp_stack.push(AstNode::new(Parens(t), pos));
             },
             &Token { typ: TokenType::OpSingle(ch), pos } => {
-                if !OPS_PREFIX.contains(&ch) {
+                if !is_prefix(ch) {
                     return Err(ParseError::Parse(format!("Expected unary operator, but got {:?}", ch)));
                 }
                 self.push_operator(Op::Prefix(ch, pos));
@@ -130,8 +157,7 @@ impl<'a> ShuntingYard<'a> {
     }
 
     fn push_operator(&mut self, op: Op) {
-        let pr = prec(&op);
-        while prec(self.top_operator()) > pr {
+        while has_greater_prec(self.top_operator(), &op) {
            self.pop_operator();
         }
         self.op_stack.push(op);
